a28e7c98200cb5fa41ba9c644614f54bffbf9049
rriver2 <rriver2@naver.com>
2022-07-11 15:24:00 +0900

    [feature] #2 : add ios App project

---
+++ Ninano/Ninano/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ninano
+//
+//  Created by 이가은 on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
