b9e2a271b33274fd9c22e65942565ed72308f118
riveris-rain <eunbkang@gmail.com>
2022-07-15 22:52:59 +0900

    [Feature] #13 : Created SearchViewController

---
+++ Ninano/Ninano/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Ninano
+//
+//  Created by Eunbee Kang on 2022/07/15.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
