71efaae54815d4be92b045fcaf777e683dda9c54
riveris-rain <eunbkang@gmail.com>
2022-07-16 00:21:02 +0900

    [Wip] #13 : Navigation title and search button

--- Ninano/Ninano/SearchViewController.swift
+++ Ninano/Ninano/SearchViewController.swift
@@ -11,7 +11,11 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func searchButton(_ sender: Any) {
+        // To be connected to SearchResultViewController
+    }
+
 }
