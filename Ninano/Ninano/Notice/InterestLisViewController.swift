1f1798484c3c29ec4ea98323c43317f7fb61a3d9
Greg Shim <hidra0321@naver.com>
2022-07-17 17:49:58 +0900

    feat: 그리드 뷰 생성
    team 문제 때문에 그려보진 못했고 이미지 삽입하고 돌리기 전에 한 것 푸쉬

--- Ninano/Ninano/Notice/InterestLisViewController.swift
+++ Ninano/Ninano/Notice/InterestLisViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class InterestLisViewController: UIViewController {
 
-    @IBOutlet weak var testTitle: UILabel!
+    @IBOutlet weak var interestCollectionView: UICollectionView!
+    
+    let gridImage = ["camera.macro.circle", "camera.macro.circle.fill", "sun.dust.fill", "cloud.moon.bolt", "hurricane", "wrench", "radio.fill"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        testTitle.text = "TEST!"
     }
 
+}
+
+extension InterestLisViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return gridImage.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NoticeGridCollectionViewCell", for: indexPath) as! NoticeGridCollectionViewCell
+        
+        cell.gridImages.image = UIImage(systemName: gridImage[indexPath.row]) ?? UIImage()
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = collectionView.frame.width / 2 - 2.0
+        let height: CGFloat = collectionView.frame.height / 4
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 2.0
+    }
+ 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2.0
+    }
+    
 }
---
+++ Ninano/Ninano/Notice/NoticeGridCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NoticeGridCollectionViewCell.swift
+//  Ninano
+//
+//  Created by Greg.G.S on 2022/07/15.
+//
+
+import UIKit
+
+class NoticeGridCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var gridImages: UIImageView!
+    
+    
+}
