86855eb44c0b2d31739f3ca1d86c6dd532ee91ec
PecanPie / Kyubo Shim <hidra0321@naver.com>
2022-07-18 11:40:11 +0900

    feat: 뷰에 따른 버튼 텍스트 변경
    하지만 약간 변화점을 두어야 할 것 같다. 둘의 차이점이 없다. 색을 바꾸던지 액션을 만들던지, 해야할 것 같다.

--- Ninano/Ninano/Notice/NoticeViewController.swift
+++ Ninano/Ninano/Notice/NoticeViewController.swift
@@ -12,12 +12,13 @@ class NoticeViewController: UIViewController {
     @IBOutlet weak var keywordContainerView: UIView!
     @IBOutlet weak var interestContainerView: UIView!
     @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var buttonState: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         keywordContainerView.alpha = 1.0
         interestContainerView.alpha = 0.0
-        
+        buttonState.tintColor = #colorLiteral(red: 0.7622407675, green: 0.1809852719, blue: 0.1365764439, alpha: 1)
     }
 
     @IBAction func didChangeIndex(_ sender: UISegmentedControl) {
@@ -25,12 +26,28 @@ class NoticeViewController: UIViewController {
         case 0:
             keywordContainerView.alpha = 1
             interestContainerView.alpha = 0
+            buttonState.setTitle("키워드 설정", for: .normal)
+
         case 1:
             keywordContainerView.alpha = 0
             interestContainerView.alpha = 1
+            buttonState.setTitle("목록 전체 삭제", for: .normal)
+//            buttonState.titleLabel?.font = UIFont.boldSystemFont(ofSize: 50)
+
         default:
             break
         }
+    }
+    
+    @IBAction func bottomTappedChange(_ sender: UIButton) {
+//        switch keywordContainerView.alpha {
+//        case 1.0:
+//
+//        case 0.0:
+//
+//        default:
+//            break
+//        }
     }
     
     @IBAction func noticeOutTapped(_ sender: UIButton) {
