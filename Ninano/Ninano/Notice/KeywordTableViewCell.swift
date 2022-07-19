1010c8b47f0dd1ad12db46f3d5925dc71d0f2d9f
PecanPie / Kyubo Shim <89404664+PecanPiePOS@users.noreply.github.com>
2022-07-19 13:53:39 +0900

    Update Ninano/Ninano/Notice/KeywordTableViewCell.swift
    Co-authored-by: Eunyeong Kim <unnnyong@gmail.com>

--- Ninano/Ninano/Notice/KeywordTableViewCell.swift
+++ Ninano/Ninano/Notice/KeywordTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class KeywordTableViewCell: UITableViewCell {
+final class KeywordTableViewCell: UITableViewCell {
 
     @IBOutlet private weak var keywordDate: UILabel!
     @IBOutlet private weak var keywordAlarmTitle: UILabel!
