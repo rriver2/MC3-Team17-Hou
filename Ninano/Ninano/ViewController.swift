f8316ef20a772642fdd3eefffcf22fbc103d3539
Asher3576 <dud3576@naver.com>
2022-07-20 09:12:52 +0900

    [feat] toast popup 버튼 기능 구현

--- Ninano/Ninano/ViewController.swift
+++ Ninano/Ninano/ViewController.swift
@@ -2,37 +2,47 @@
 //  ViewController.swift
 //  Ninano
 //
-//  Created by 이가은 on 2022/07/11.
+//  Created by 임영후 on 2022/07/11.
 //
 
 import UIKit
+import NotificationToast
 
 class ViewController: UIViewController {
 
+    // MARK: 포스터 홈페이지 링크 버튼
     @IBOutlet weak var linkBtn: UIButton!
+    // MARK: 포스터 일정 추가 버튼
     @IBOutlet weak var likeBtn: UIButton!
-    
+    // MARK: 포스터 공유 시트 버튼
     @IBAction func shareSheetBtn(_ sender: Any) {
         presentShareSheet()
     }
-    // 모달 뷰 코드로 구현 시
-//    @IBAction func addCalendarBtn() {
-//        guard let calendarBtn = storyboard?.instantiateViewController(identifier: "modalCalendarVC") as? ModalCalendarViewController else {
-//            return
-//        }
-//        present(calendarBtn, animated: true)
-//    }
+    // MARK: 토스트 팝업 커튼
+    @IBAction func toastPopUp(_ sender: Any) {
+        let toast = ToastView(
+            title: "토요국악",
+            titleFont: .systemFont(ofSize: 13, weight: .regular),
+            subtitle: "일정추가 완료 되었습니다.",
+            subtitleFont: .systemFont(ofSize: 11, weight: .light),
+            icon: UIImage(systemName: "calendar.badge.plus"),
+            iconSpacing: 16,
+            position: .top
+        )
+        toast.show()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         linkBtn.layer.cornerRadius = 20
-        likeBtn.layer.cornerRadius = 20    }
+        likeBtn.layer.cornerRadius = 20
+    }
     
     private func presentShareSheet() {
         guard let image = UIImage(systemName: "bell"), let url = URL(string: "https://www.daejeon.go.kr/kmusic/kmsPublicPerformanceView.do?pblprfrInfoId=1096&menuSeq=6400&searchAllPblprfrAt=&searchPastPblprfrAt=&searchPblprfrFormClCode=&pageIndex=") else {
             return
         }
-        let shareSheetVC = UIActivityViewController(activityItems: [image,url], applicationActivities: nil)
+        let shareSheetVC = UIActivityViewController(activityItems: [image, url], applicationActivities: nil)
         present(shareSheetVC, animated: true)
     }
 }
