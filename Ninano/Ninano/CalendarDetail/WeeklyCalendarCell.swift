a07d4e34aae65cc31151b1b03cd234acb3fe4d41
riveris-rain <eunbkang@gmail.com>
2022-07-23 02:54:34 +0900

    [Chore] #27 final classes

--- Ninano/Ninano/CalendarDetail/WeeklyCalendarCell.swift
+++ Ninano/Ninano/CalendarDetail/WeeklyCalendarCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class WeeklyCalendarCell: UICollectionViewCell {
+final class WeeklyCalendarCell: UICollectionViewCell {
     @IBOutlet weak var dayNameLabel: UILabel!
     @IBOutlet weak var dateNumberLabel: UILabel!
 }
