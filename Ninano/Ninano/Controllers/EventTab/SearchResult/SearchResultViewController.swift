e4e7e37bb4bfa3a1622b57a7e1df291e6b6a2e26
rriver2 <rriver2@naver.com>
2022-07-28 12:38:08 +0900

    [feat] make searchCatagoryView

--- Ninano/Ninano/Controllers/EventTab/SearchResult/SearchResultViewController.swift
+++ Ninano/Ninano/Controllers/EventTab/SearchResult/SearchResultViewController.swift
@@ -17,6 +17,12 @@ final class SearchResultViewController: UIViewController {
     private var isNotificationButtonSelected = false
     
     var eventList: [Event] = []
+    var viewCatagory: SearchDetailCatagory?
+    
+    enum SearchDetailCatagory {
+        case searchCatagory(navigationTitle: String)
+        case searchResult
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +36,21 @@ final class SearchResultViewController: UIViewController {
         let uiImage = UIImage(systemName: "chevron.left")
         let undo = UIBarButtonItem(image: uiImage, style: .plain, target: self, action: #selector(didTapBackButton))
         self.navigationItem.leftBarButtonItem = undo
-        let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 320, height: 0))
-        searchBar.placeholder = "Search User"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: searchBar)
+        
+        switch viewCatagory {
+            case .searchCatagory(let navigationTitle):
+                let searchCatagoryTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 20))
+                searchCatagoryTitle.textAlignment = .center
+                searchCatagoryTitle.font = UIFont.systemFont(ofSize: 20)
+                    searchCatagoryTitle.text = navigationTitle
+                self.navigationItem.titleView = searchCatagoryTitle
+            case .searchResult:
+                let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 320, height: 0))
+                searchBar.placeholder = "Search User"
+                self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: searchBar)
+            case .none:
+                print("viewCatagory error")
+        }
         
         keywordNotification.layer.cornerRadius = 15
     }
