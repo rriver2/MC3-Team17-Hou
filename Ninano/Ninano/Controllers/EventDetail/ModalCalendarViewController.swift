764ba2b15c1175a3a9aae7e49cefc16802857b8f
Asher3576 <dud3576@naver.com>
2022-08-01 00:36:55 +0900

    [feat] EventDetailView  일정추가/저장 기능

--- Ninano/Ninano/Controllers/EventDetail/ModalCalendarViewController.swift
+++ Ninano/Ninano/Controllers/EventDetail/ModalCalendarViewController.swift
@@ -8,16 +8,35 @@
 import UIKit
 
 class ModalCalendarViewController: UIViewController {
-
+    
+    var event: Event?
+    var parentVC: UIViewController?
+    private var reserveManager = ReserveManager.shared
+    private var selectedDate: Date?
+    
     @IBOutlet weak var calendarFrame: UIView!
     @IBOutlet weak var addSchedule: UIButton!
     @IBAction func cancleBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func datePicked(_ sender: UIDatePicker) {
+        selectedDate = sender.date
+    }
+    
+    @IBAction func confirmBtnPressed(_ sender: UIButton) {
+        if let event = event {
+            let reserveModel = ReserveModel(reserveDate: selectedDate, isReserved: true, url: event.URL!)
+            reserveManager.insertReserve(reserveModel)
+        }
+        guard let vc = self.parentVC as? EventDetailViewController else { return }
+        vc.drawReserveButton()
+        self.dismiss(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         calendarFrame.layer.cornerRadius = 10
         addSchedule.layer.cornerRadius = 10
     }
